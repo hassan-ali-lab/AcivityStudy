2d1ebf3599fed1a692315a1eb957d69ed3233a22
Hassan Ali <hassanali.bscsf19@iba-suk.edu.pk>
2022-12-12 17:49:52 +0500

    Initial commit

---
+++ app/src/main/java/com/example/acivitystudy/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.acivitystudy
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+    }
+
+
+}
